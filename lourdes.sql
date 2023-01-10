9535a1871a47929fad4667f4930e00c94a2af73b
sidneyboy <100180350+sidneyboy@users.noreply.github.com>
2023-01-10 19:02:32 +0800

    updated

--- lourdes.sql
+++ lourdes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2022 at 12:07 AM
+-- Generation Time: Jan 10, 2023 at 12:00 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.0.19
 
@@ -40,8 +40,7 @@ CREATE TABLE `abouts` (
 --
 
 INSERT INTO `abouts` (`id`, `about`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'asdasd', '167071866737.png', '2022-12-10 16:31:07', '2022-12-10 16:31:07'),
-(2, 'The soul of Nikan resort can meet all of your gathering expectations in one place. You could be forced to partake in entertainment, food, beverages, and other activities without leaving the premises. The Nikan Resort also offers fantastic recreational amenities including swimming pools, function hall, and locations for relaxation.', '167071870627.png', '2022-12-10 16:31:46', '2022-12-10 16:31:46');
+(3, 'The soul of Nikan resort can meet all of your gathering expectations in one place. You could be forced to partake in entertainment, food, beverages, and other activities without leaving the premises. The Nikan Resort also offers fantastic recreational amenities including swimming pools, function hall, and locations for relaxation.\r\n\r\nsample', '167093266010.png', '2022-12-13 03:55:12', '2022-12-13 03:57:40');
 
 -- --------------------------------------------------------
 
@@ -65,15 +64,9 @@ CREATE TABLE `accomodations` (
 --
 
 INSERT INTO `accomodations` (`id`, `type_id`, `title`, `description`, `created_at`, `updated_at`, `status`, `image`) VALUES
-(6, 'room', 'Room 1', 'Description', '2022-12-10 15:36:11', '2022-12-10 15:36:11', NULL, '167071537115.jpg'),
-(7, 'room', 'Room 2', 'Description', '2022-12-10 15:36:24', '2022-12-10 15:36:24', NULL, '167071538453.jpg'),
-(8, 'room', 'Room 3', 'Description', '2022-12-10 15:36:36', '2022-12-10 15:36:36', NULL, '167071539692.jpg'),
-(9, 'meals', 'Meal 1', 'Desc', '2022-12-10 15:50:17', '2022-12-10 15:50:17', NULL, '167071621762.jpg'),
-(10, 'meals', 'Meal 2', 'Desc', '2022-12-10 15:50:27', '2022-12-10 15:50:27', NULL, '167071622735.jpg'),
-(11, 'meals', 'Meal 3', 'Desc', '2022-12-10 15:50:38', '2022-12-10 15:50:38', NULL, '167071623884.png'),
-(12, 'liqour', 'Service 1', 'Desc', '2022-12-10 15:56:23', '2022-12-10 15:56:23', NULL, '167071658317.jpg'),
-(13, 'liqour', 'Service 2', 'Desc', '2022-12-10 15:56:34', '2022-12-10 15:56:34', NULL, '167071659486.jpg'),
-(14, 'liqour', 'Service 3', 'Desc', '2022-12-10 15:56:45', '2022-12-10 15:56:45', NULL, '167071660592.jpg');
+(15, 'room', 'Room 1', 'Desc', '2022-12-13 03:58:16', '2022-12-13 04:33:04', 'activated', '167093269632.jpg'),
+(16, 'room', 'Room 2', 'Desc', '2022-12-13 03:58:35', '2022-12-13 04:31:27', 'activated', '167093271573.jpg'),
+(17, 'liqour', 'Tanduay', 'Desc', '2022-12-13 03:59:32', '2022-12-13 04:33:08', 'activated', '167093277214.jpg');
 
 -- --------------------------------------------------------
 
@@ -108,9 +101,9 @@ CREATE TABLE `carousels` (
 --
 
 INSERT INTO `carousels` (`id`, `image`, `status`, `created_at`, `updated_at`) VALUES
-(1, '167071921354.jpg', 'active', '2022-12-10 16:40:13', '2022-12-10 16:40:13'),
-(2, '167071921362.jpg', 'active', '2022-12-10 16:40:13', '2022-12-10 16:40:13'),
-(3, '167071921392.jpg', 'active', '2022-12-10 16:40:13', '2022-12-10 16:40:13');
+(4, '167093252634.jpg', 'active', '2022-12-13 03:55:26', '2022-12-13 03:56:38'),
+(5, '16709325262.jpg', 'active', '2022-12-13 03:55:26', '2022-12-13 03:55:26'),
+(6, '167093252697.jpg', 'qqweqe@gmail.com', '2022-12-13 03:55:26', '2022-12-13 04:18:37');
 
 -- --------------------------------------------------------
 
@@ -134,8 +127,7 @@ CREATE TABLE `contact_uses` (
 --
 
 INSERT INTO `contact_uses` (`id`, `name`, `email`, `number`, `message`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'asdasdasd', 'dasdasd@gmail.com', '09533844872', 'asdasdMessage', 'replied', '2022-12-12 03:35:42', '2022-12-12 04:05:29'),
-(2, 'asdasdasd', 'dasdasd@gmail.com', '09533844872', 'asdasdMessage', 'Pending', '2022-12-12 03:36:14', '2022-12-12 03:36:14');
+(3, 'Sidney', 'salazarjohnsidney@gmail.com', '09533844872', 'Tag pila ang reservation?', 'Pending', '2022-12-13 04:01:10', '2022-12-13 04:01:10');
 
 -- --------------------------------------------------------
 
@@ -181,7 +173,11 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (10, '2022_12_11_003707_create_carousels_table', 8),
 (11, '2022_12_12_112657_create_contact_uses_table', 9),
 (12, '2022_12_12_122742_create_reservations_table', 10),
-(13, '2022_12_12_134439_add_payment', 11);
+(13, '2022_12_12_134439_add_payment', 11),
+(14, '2022_12_14_122220_add_dates', 12),
+(15, '2022_12_15_134342_create_reservations_details_table', 13),
+(16, '2022_12_17_021112_add_status_to_details', 14),
+(17, '2023_01_07_114017_add_downpayment', 15);
 
 -- --------------------------------------------------------
 
@@ -211,21 +207,48 @@ CREATE TABLE `reservations` (
   `receipt` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `date_from` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date_to` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `date` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `payment` double(15,2) DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `reservations`
 --
 
-INSERT INTO `reservations` (`id`, `first_name`, `middle_name`, `last_name`, `email`, `number`, `receipt`, `status`, `date_from`, `date_to`, `date`, `created_at`, `updated_at`, `payment`) VALUES
-(1, 'sadasd', 'qweqwe', 'aczxczxc', 'sample@gmail.com', '09533844872', '167084887530.png', 'Paid', '2022-12-12', '2022-12-13', '2022-12-12 20:41:15', '2022-12-12 12:41:15', '2022-12-12 14:24:51', 2000.00),
-(2, 'sample', 'asdasd', 'qweqwe', 'qsadasd@gmail.com', '09533844872', '167084892125.png', 'For Final Payment', '2022-12-12', '2022-12-12', '2022-12-12 20:42:01', '2022-12-12 12:42:01', '2022-12-12 14:18:15', 5000.00),
-(3, 'weqwe', 'qweqwe', 'qweqwe', 'qweqwe@gmail.com', '09533844872', '167084901223.png', 'Pending', '2022-12-12', '2022-12-12', '2022-12-12 20:43:32', '2022-12-12 12:43:32', '2022-12-12 12:43:32', NULL);
+INSERT INTO `reservations` (`id`, `first_name`, `middle_name`, `last_name`, `email`, `number`, `receipt`, `status`, `date_from`, `date`, `created_at`, `updated_at`) VALUES
+(18, 'sample', 'sample', 'sample', 'sample@gmail.com', '09533844872', '167309240831.png', 'Paid', '2023-01-11', '2023-01-07 19:53:28', '2023-01-07 11:53:29', '2023-01-09 04:20:01'),
+(19, 'sample3', 'sample3', 'sample3', 'sample3@gmail.com', '09533844872', '167309245177.png', 'Paid', '2023-01-18', '2023-01-07 19:54:11', '2023-01-20 11:54:11', '2023-01-09 04:21:21'),
+(20, 'sample', 'sample', 'sample', 'sample@gmail.com', '09533844872', '167309319740.png', 'Cancelled', '2023-01-25', '2023-01-07 20:06:37', '2023-01-07 12:06:37', '2023-01-07 04:06:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservations_details`
+--
+
+CREATE TABLE `reservations_details` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `reservation_id` bigint(20) NOT NULL,
+  `payment` double(15,2) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `downpayment` double(15,4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `reservations_details`
+--
+
+INSERT INTO `reservations_details` (`id`, `reservation_id`, `payment`, `created_at`, `updated_at`, `status`, `downpayment`) VALUES
+(26, 19, NULL, '2023-01-07 03:54:18', '2023-01-07 03:54:18', 'Paid Downpayment', 500.0000),
+(27, 18, NULL, '2023-01-07 11:54:40', '2023-01-07 11:54:40', 'Paid Downpayment', 500.0000),
+(28, 19, 1000.00, '2023-01-04 11:54:55', '2023-01-07 11:54:55', 'Partial Payment', NULL),
+(29, 19, 800.00, '2023-01-19 11:55:07', '2023-01-07 11:55:07', 'Partial Payment', NULL),
+(30, 18, 800.00, '2023-01-18 11:56:14', '2023-01-07 11:56:14', 'Partial Payment', NULL),
+(31, 18, 4700.00, '2023-01-09 04:20:02', '2023-01-09 04:20:02', 'Paid', NULL),
+(32, 19, 3700.00, '2023-01-09 04:21:21', '2023-01-09 04:21:21', 'Paid', NULL);
 
 -- --------------------------------------------------------
 
@@ -239,15 +262,6 @@ CREATE TABLE `types` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `types`
---
-
-INSERT INTO `types` (`id`, `type`, `created_at`, `updated_at`) VALUES
-(6, 'Rooms', '2022-12-06 05:10:30', '2022-12-06 05:10:54'),
-(7, 'Services', '2022-12-06 05:10:59', '2022-12-06 05:10:59'),
-(8, 'Meals', '2022-12-06 05:11:04', '2022-12-06 05:11:04');
 
 -- --------------------------------------------------------
 
@@ -333,6 +347,12 @@ ALTER TABLE `reservations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `reservations_details`
+--
+ALTER TABLE `reservations_details`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `types`
 --
 ALTER TABLE `types`
@@ -353,13 +373,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `abouts`
 --
 ALTER TABLE `abouts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `accomodations`
 --
 ALTER TABLE `accomodations`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `accomodation_images`
@@ -371,13 +391,13 @@ ALTER TABLE `accomodation_images`
 -- AUTO_INCREMENT for table `carousels`
 --
 ALTER TABLE `carousels`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `contact_uses`
 --
 ALTER TABLE `contact_uses`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -389,13 +409,19 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `reservations_details`
+--
+ALTER TABLE `reservations_details`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `types`
